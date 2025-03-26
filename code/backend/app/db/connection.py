ece60d5ed34944e2bf6afe6c31a883046cc2f2ec
samriddhiag <samriddhi.agarwal2019a@vitstudent.ac.in>
2025-03-26 21:37:35 +0530

    removed old code

--- code/backend/app/db/connection.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-# MongoDB connection
-client = MongoClient(os.getenv("MONGODB_URL", "mongodb://localhost:27017"))
-db = client[os.getenv("DB_NAME", "finpersona")]
